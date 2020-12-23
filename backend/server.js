87d3fb722762cb02e9beac346456146b5406e354
ad-fleming <adfleming9@gmail.com>
2020-12-23 10:56:31 -0500

    added MONGODB_URI to env file and added a config file to backend to establish connection

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,33 @@
-// Converted to uses ES modules syntax. Added "type": "module" to package json. 
+// Converted to uses ES modules syntax. Added "type": "module" to package json.
 // When importing files, make sure to include the file extension "example.js"
 
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-app.get('/api/products', (req,res)=> {
+app.get('/api/products', (req, res) => {
   res.json(products)
 })
 
-app.get('/api/products/:id', (req,res)=> {
-  const product = products.find(p => p._id === req.params.id)
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
