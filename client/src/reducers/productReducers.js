0f8231a8f4975e0d281c1a2096a64e573489bb58
ad-fleming <adfleming9@gmail.com>
2020-12-23 15:53:40 -0500

    added product details reducer and action

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -1,9 +1,16 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
+import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from '../constants/productConstants'
 
 // Product List -- Handles state for the product list on Homepage
 
-export const productListReducer = (state = { products: [] }, action) =>{
-  switch(action.type){
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
@@ -15,3 +22,18 @@ export const productListReducer = (state = { products: [] }, action) =>{
   }
 }
 
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
