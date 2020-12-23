073dbba9343353b0cd5523bf6211a0747a4d4481
ad-fleming <adfleming9@gmail.com>
2020-12-23 12:28:59 -0500

    added middleware for error handling and wrapped async routes with express-async-handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -16,15 +17,19 @@ const app = express()
 
 app.use('/api/products', productRoutes)
 
+app.use(notFound)
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-
-
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
