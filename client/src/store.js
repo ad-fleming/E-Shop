0431eb172996893eca30631a1f197d0184cc05df
ad-fleming <adfleming9@gmail.com>
2020-12-23 16:36:23 -0500

    cart reducer and addToCart action created as well as inital state updated for cart in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,24 +1,40 @@
-// This is where we'll connect any reducers or middleware 
+// This is where we'll connect any reducers or middleware
 
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage
+  }
+}
 
 const middleware = [thunk]
 
-// pass in: 
+// pass in:
 // 1) reducer
 // 2) initial state
 // 3) middleware
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)) )
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
-export default store 
+export default store
