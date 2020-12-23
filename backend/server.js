b7b922fc09501a006f56d330405129301f881d58
ad-fleming <adfleming9@gmail.com>
2020-12-23 11:01:37 -0500

    installed color.js for console messages

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -29,5 +30,5 @@ app.get('/api/products/:id', (req, res) => {
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
