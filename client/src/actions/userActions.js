cc62b2971f0d2f29aff36d434f40621a661b0209
ad-fleming <adfleming9@gmail.com>
2020-12-23 23:44:28 -0500

    created registerstration screen, reducer, action. Successfully posting to database

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,4 +1,12 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_REQUEST } from '../constants/userConstants'
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from '../constants/userConstants'
 import axios from 'axios'
 
 export const login = (email, password) => async (dispatch) => {
@@ -20,8 +28,8 @@ export const login = (email, password) => async (dispatch) => {
     )
 
     dispatch({
-      type:USER_LOGIN_SUCCESS,
-      payload: data
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
     })
 
     localStorage.setItem('userInfo', JSON.stringify(data))
@@ -36,7 +44,7 @@ export const login = (email, password) => async (dispatch) => {
   }
 }
 
-export const logout = () => (dispatch) =>{
+export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
 }
@@ -60,13 +68,13 @@ export const register = (name, email, password) => async (dispatch) => {
     )
 
     dispatch({
-      type:USER_REGISTER_SUCCESS,
-      payload: data
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
     })
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
-      payload: data
+      payload: data,
     })
 
     localStorage.setItem('userInfo', JSON.stringify(data))
