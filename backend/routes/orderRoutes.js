b6934e0e3c8b775aa2e8807ae62a2df81ee619ee
ad-fleming <adfleming9@gmail.com>
2020-12-28 14:14:38 -0500

    added controller and route for getting an order by id

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 const router = express.Router()
 import {
-  addOrderItems
+  addOrderItems,
+  getOrderByID
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
-
+router.route('/:id').get(protect, getOrderByID)
 
 export default router
--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -1,9 +1,9 @@
+import axios from 'axios'
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_SUCCESS,
-  ORDER_CREATE_REQUESST,
+  ORDER_CREATE_REQUEST,
 } from '../constants/orderConstants'
-
 
 export const createOrder = (order) => async (dispatch, getState) => {
   try {
@@ -22,7 +22,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.put(`/api/users/profile`, user, config)
+    const { data } = await axios.post(`/api/orders`, order, config)
 
     dispatch({
       type: ORDER_CREATE_SUCCESS,
