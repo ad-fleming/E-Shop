80f2f38af07328771ab9edd49057aea823196972
ad-fleming <adfleming9@gmail.com>
2020-12-28 10:16:19 -0500

    created payment screen

--- client/src/components/CheckoutSteps.js
+++ client/src/components/CheckoutSteps.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Nav, nav } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
-    <Nav claseName='justify-content-center mb-4'>
+    <Nav className='justify-content-center mb-4'>
       <Nav.Item>
         {step1 ? (
           <LinkContainer to='/login'>
