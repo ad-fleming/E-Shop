af9778182eb78bf13039902312409faf8ec83b10
ad-fleming <adfleming9@gmail.com>
2020-12-31 08:36:04 -0500

    added route to get all users and created middleware function to check for admin status to further restrict the route

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,4 +28,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-export { protect }
+const admin = (req, res, next) => {
+  if(req.user && req.user.isAdmin){
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Admin authorization required')
+  }
+}
+
+export { protect, admin }
