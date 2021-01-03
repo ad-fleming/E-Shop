e614df57d58372f6a084201e2eb1dbc38805481f
ad-fleming <adfleming9@gmail.com>
2021-01-03 05:09:47 -0500

    added installed multer for image file uploads and added upload route.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // Converted to uses ES modules syntax. Added "type": "module" to package json.
 // When importing files, make sure to include the file extension "example.js"
-
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -10,6 +10,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -22,10 +23,14 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
