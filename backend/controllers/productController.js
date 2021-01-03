136fa071ee134a897cebea61fa2cb0d859b3a53e
ad-fleming <adfleming9@gmail.com>
2021-01-03 03:31:42 -0500

    added controller and routes for creating and updating products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,7 +12,6 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products)
 })
 
-
 // @desc Fetch product by ID
 // @route GET /api/products/:id
 // @access Public
@@ -32,15 +31,69 @@ const getProductById = asyncHandler(async (req, res) => {
 // @access PRIVATE/ADMIN
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
-  // We could add a conditional to match the req.user._id to the product.user._id if we wanted to 
+  // We could add a conditional to match the req.user._id to the product.user._id if we wanted to
   // restrict to only deleting products that users created.
   if (product) {
     await product.remove()
-    res.json({message: 'Product removed'})
+    res.json({ message: 'Product removed' })
   } else {
     res.status(404)
     throw new Error('Product not found')
   }
 })
 
-export { getProductById, getProducts, deleteProduct }
+// @desc Create a product
+// @route POST /api/products
+// @access PRIVATE/ADMIN
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+
+  res.status(201).json(createdProduct)
+})
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access PRIVATE/ADMIN
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product){
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error ('Product not found')
+  }
+})
+
+export { getProductById, getProducts, deleteProduct, createProduct, updateProduct }
