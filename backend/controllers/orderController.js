ef0c6402df437557cc2d701a94e6a1777dca363b
ad-fleming <adfleming9@gmail.com>
2021-01-04 22:39:19 -0500

    added back and front end functionaility for admin product and order list. Admins can now mark orders as delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -73,7 +73,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.body.payer.email_address
+      email_address: req.body.payer.email_address,
       // IF YOU ADD ANOTHER PAYMENT GATEWAY (STRIPE), it's possible that other info
       // may need to be added here
     }
@@ -92,8 +92,44 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @access Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ user: req.user._id})
+  const orders = await Order.find({ user: req.user._id })
   res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
+// @des GET all orders
+// @route GET /api/orders
+// @access Private/Admin
+
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find().populate('user', 'id name')
+  res.json(orders)
+})
+
+// @des Update order to delivered
+// @route GET /api/orders/:id/deliver
+// @access Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders,
+}
