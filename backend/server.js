0784e1266c31c1f7882b44b3a0f6348246b28a1e
ad-fleming <adfleming9@gmail.com>
2021-01-05 17:47:44 -0500

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB()
 
 const app = express()
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
@@ -38,15 +38,23 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
 app.use(notFound)
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
 
 app.listen(
   PORT,
