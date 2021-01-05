f046e2e0e39e04030689d2cf5f9dd5ef67aa85f2
ad-fleming <adfleming9@gmail.com>
2021-01-05 18:08:58 -0500

    corrected typo on product rating section

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -177,8 +177,8 @@ const ProductScreen = ({ history, match }) => {
                           <option value='1'>1 - Poor</option>
                           <option value='2'>2 - Fair</option>
                           <option value='3'>3 - Good</option>
-                          <option value='4'>1 - Very Good</option>
-                          <option value='5'>1 - Excellent</option>
+                          <option value='4'>4 - Very Good</option>
+                          <option value='5'>5 - Excellent</option>
                         </Form.Control>
                       </Form.Group>
                       <Form.Group controlId='comment'>
