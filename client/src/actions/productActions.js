5f5e368dec19384bb31d1bcd40a9067a4eaf337c
ad-fleming <adfleming9@gmail.com>
2021-01-05 13:39:25 -0500

    created searchbox component and added search funtionality

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -21,12 +21,12 @@ import {
 } from '../constants/productConstants'
 
 // Here, we are creating action creator functions and using thunk to allow us to have async functions within the function
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (keyword = '') => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
 
     // destructuring data from the response object
-    const { data } = await axios.get('/api/products')
+    const { data } = await axios.get(`/api/products?keyword=${keyword}`)
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
